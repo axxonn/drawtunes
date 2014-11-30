411d0d445f3e70126cc9c311142ce496bb3f45ed
Richard Wu <rwu3018@gmail.com>
2014-11-30 04:44:07 -0500

    Refactor to actually look like something.

---
+++ src/main.py
@@ -0,0 +1,68 @@
+from PIL import Image
+from midiutil.MidiFile import MIDIFile
+import webbrowser
+
+def get_pixels(filename):
+    im = Image.open(filename)
+    
+    pixels = list(im.getdata())
+    width, height = im.size
+    # partition into columns
+    pixel_cols = []
+    for i in xrange(width):
+        pixel_cols.append([])
+        for j in xrange(height):
+            value = pixels[j * width + i][0]
+            if value == 255:
+                pixel_cols[i].append(0)
+            else:
+                pixel_cols[i].append(1)
+    
+    #for i in xrange(len(pixel_cols)):
+    #    for j in xrange(len(pixel_cols[0])):
+    #        print pixel_cols[i][j],
+    #    print '\n'
+    
+    return pixel_cols
+
+def convert_to_music(pixels):
+    # Create the MIDIFile Object with 1 track
+    MyMIDI = MIDIFile(1)
+    
+    # Tracks are numbered from zero. Times are measured in beats.
+    track = 0   
+    time = 0
+    
+    # Add track name and tempo.
+    MyMIDI.addTrackName(track,time,"Sample Track")
+    MyMIDI.addTempo(track,time,120)
+    
+    track = 0
+    channel = 0
+    pitch = 0
+    time = 0
+    duration = 0.5
+    volume = 100
+    
+    width = len(pixels)
+    height = len(pixels[0])
+    for i in xrange(width):
+        for j in xrange(height):
+            temp = 0
+            if pixels[i][j] == 1 and not (i > 0 and pixels[i-1][j] == 1):
+                while i + temp + 1 < width and pixels[i + temp + 1][j] == 1:
+                    temp += 1
+                MyMIDI.addNote(0, 0, 100 - j, time, duration * (1 + temp), 100)
+        time += duration
+    
+    return MyMIDI
+
+def main():
+    pixels = get_pixels('input.png')
+    midi = convert_to_music(pixels)
+    binfile = open("output2.mid", 'wb')
+    midi.writeFile(binfile)
+    binfile.close()
+    webbrowser.open('output2.mid')
+
+main()
