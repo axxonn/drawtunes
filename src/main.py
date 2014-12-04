3f901bbb9567e53ef245abf733e07ff0a29f344f
Richard Wu <rwu3018@gmail.com>
2014-12-04 02:04:37 -0500

    Beautiful chord correction implemented.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def get_pixels(filename):
     """Returns: a 2D list that contains tuples (RBG values)
     Precondition: filename is a string that corresponds to a png file"""
     im = Image.open(filename)
-    
+
     pixels = list(im.getdata())
     width, height = im.size
     # partition into columns
@@ -17,29 +17,29 @@ def get_pixels(filename):
         for j in xrange(height):
             value = pixels[j * width + i]
             pixel_cols[i].append(value)
-    
+
     # for i in xrange(len(pixel_cols)):
     #    for j in xrange(len(pixel_cols[0])):
     #        print pixel_cols[i][j],
     #    print '\n'
-    
+
     return pixel_cols
 
 def convert_to_music(midi, pixels, instrument, tempo = 120):
     """Writes the given pixels to the midi on the given track.
     #May specify duration of notes."""
     MyMIDI = midi
-    
+
     # Tracks are numbered from zero. Times are measured in beats.
-    #track = 0   
+    #track = 0
     time = 0
-    
+
     # Add track name and tempo.
     #MyMIDI.addTrackName(track,time,"Sample Track")
     MyMIDI.addTempo(instrument,time,tempo)
-    
+
     duration = 0.5
-    
+
     width = len(pixels)
     height = len(pixels[0])
     for i in xrange(width):
@@ -50,12 +50,75 @@ def convert_to_music(midi, pixels, instrument, tempo = 120):
                 temp = 0
                 while i + temp + 1 < width and pixels[i + temp + 1][j] == 1:
                     temp += 1
-                pitch = int((height-j)/float(height) * 88) + 21
+                pitch = (88 - j) + 21 # int((height-j)/float(height) * 88) + 21
                 # parameters: track, channel, pitch, time, duration, volume
                 MyMIDI.addNote(instrument, instrument, pitch, time, duration * (1 + temp), 100)
         time += duration
-    
+
     #return MyMIDI
+
+
+def normalize_height(pixellist):
+    """Returns: a 2D list of height 88 that contains tuples (RGB values)
+    Precondition: pixellist is a 2D list of random height and width"""
+    pixel88list = []
+    for column in range(len(pixellist)):
+        newcolumn = []
+        for row in range(88): # creates columns of 88 white pixels
+            newcolumn.append((255, 255, 255)) # appends a white pixel
+        pixel88list.append(newcolumn) # adds column to new 2D list
+    for column in range(len(pixellist)):
+        for row in range(len(pixellist[column])):
+            if pixellist[column][row][:3] != (255, 255, 255):
+                height = int((row*1.0)/len(pixellist[column]) * 88)
+                pixel88list[column][height] = pixellist[column][row]
+    return pixel88list
+
+def fix_chords(pixel88list):
+    """Returns: a beautiful version of pixel88list. Notes are shifted so that they do not clash within chords."""
+    for column in range(len(pixel88list)):
+        notes = {} # set of pitches in a given column
+        #for row in xrange(len(pixel88list[column]))[1:]: # [1:] because only need to start checking on second note
+        for row in range(len(pixel88list[column]))[:-1]:
+            pixel = pixel88list[column][row][:3]
+            if pixel != (255, 255, 255):
+                pitch = 88 - row
+                index = row # index for keeping track of shifted note
+                for i in notes:
+                    while is_clashing(i, pitch):
+                        if index + 1 >= len(pixel88list[column]):
+                            print 'oh no'
+                            break
+                        pitch -= 1
+                        pixel88list[column][index+1] = pixel
+                        pixel88list[column][index] = (255, 255, 255)
+                        index += 1
+                notes[pitch] = 0  # 0 is placeholder value, just need key
+    return pixel88list
+
+
+        # for row in reversed(xrange(len(pixel88list[column]))):
+        #     pixel = pixel88list[column][row][:3]
+        #     if pixel != (255, 255, 255):
+        #         resolved = False
+        #         while not resolved:
+        #             resolved = True
+        #             for i in xrange(len(pixel88list[column])):
+        #                 if pixel88list[column][i][:3] != (255, 255, 255) and is_clashing(row, i):
+        #                     pixel88list[column][row] = (255, 255, 255)
+        #                     resolved = False
+
+def is_clashing(note1, note2):
+    """Returns: whether note1 and note2 clash, i.e. whether they are one or two half-steps away from each other
+    Precondition: note1, note2 are pitches, 0-88 (correspond to piano keyboard)"""
+    distance = min((note1-note2)%12,(note2-note1)%12)
+    return 0 < distance < 3
+    # dist1 = (given-shifter)%12
+    # if 0 < dist1 < 3:
+    #     return -(3-dist1)
+    # dist2 = (shifter-given)%12
+    # if 0 < dist2 < 3:
+    #     return 3-dist2
 
 
 def get_colors(pixellist):
@@ -105,35 +168,18 @@ def rgb_to_hue(color):
     return hue
 
 
-def normalize_height(pixellist):
-    """Returns: a 2D list of height 88 that contains tuples (RGB values)
-    Precondition: pixellist is a 2D list of random height and width"""
-    newpixellist = []
-    for column in range(len(pixellist)): 
-        newcolumn = []
-        for row in range(88): # creates columns of 88 white pixels
-            newcolumn.append((255, 255, 255)) # appends a white pixel
-        newpixellist.append(newcolumn) # adds column to new 2D list
-    for column in range(len(pixellist)):
-        for row in range(len(pixellist[column])):
-            if pixellist[column][row][:3] != (255, 255, 255): 
-                height = int((row*1.0)/len(pixellist[column]) * 88)
-                newpixellist[column][height] = pixellist[column][row] 
-    return newpixellist
-
-
 def main():
     filename = raw_input('Please enter the name of your image file (don\'t include .png): ')
     pixels = get_pixels(filename + '.png')
     pixels88 = normalize_height(pixels)
+    pixels88 = fix_chords(pixels88)
     colors = get_colors(pixels88)
     while len(colors) > 15:
-        sys.stdout.write('This image has too many colors.')
+        sys.stdout.write('This image has too many colors. ')
         filename = raw_input('Please enter the name of your image file (don\'t include .png): ')
         pixels = get_pixels(filename + '.png')
         pixels88 = normalize_height(pixels)
         colors = get_colors(pixels88)
-    print colors
     track = 0
     # Create the MIDIFile Object with 1 track
     midi = MIDIFile(len(colors))
@@ -141,8 +187,10 @@ def main():
         instrument = int((color[0]*100+color[1]*10+color[2]) / (28305/127))
         midi.addProgramChange(track, track, 0, instrument)
         colors[color] = create_masterlist(color, pixels88)
-        convert_to_music(midi, colors[color], track)
+        convert_to_music(midi, colors[color], track, tempo = 240)
         track += 1
+        print `color` + ': ' + `instrument`
+    filename = 'beautiful_' + filename
     binfile = open(filename + ".mid", 'wb')
     midi.writeFile(binfile)
     binfile.close() # no idea if this is necessary or not
